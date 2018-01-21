a42725d748030e67cc60885e420767ab0c22487d
Koke_Cacao <1412042720@qq.com>
2018-01-21 02:06:45 -0500

    Move repo to private
    Add TeleOp
    Add KokiOp
    Add library OpenCV
    Add .gitignore

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KokiAuto.java
@@ -0,0 +1,156 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by Koke_Cacao on 2018/1/18.
+ */
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.vuforia.HINT;
+import com.vuforia.Vuforia;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
+@Autonomous(name = "KokiAuto", group = "Autonomous")
+public class KokiAuto extends LinearOpMode {
+    private VuforiaLocalizer vuforiaLocalizer;
+    private VuforiaLocalizer.Parameters parameters;
+    private VuforiaTrackables visionTargets;
+    private VuforiaTrackable target;
+    private VuforiaTrackableDefaultListener listener;
+
+    private OpenGLMatrix lastKnownLocation;
+    private OpenGLMatrix phoneLocation;
+
+    private static final String VUFORIA_KEY = "AYVp8HD/////AAAAmbWvbJCPAUEivTbJDYmkKlUhuPRlEN5MxRGtGpK68YAYgdTUSycNhLm/AQ2nxYbFwiX+eiXtdzMQg/h0/OO0uHdiq2AGB9qus774oqnqQ2DrzfdUARClxtcnFwJw3Ba/tyvP/gxWjMWetKcwfdDAjD+dilVMrqS7ePsZZPzjSaNB/kjaP3yQRTN1D/050KdnxwKicMkqhulqKv1miESfNBm7qQd3h9FZJoVZumqfytS7pMmqAjvSN7TGcQw7vxw7DJAECvRfoFhuszWNjwcF3rwRsQEXr1jynbJvhh8z4SJdJDqIK4EEroLLSpHVTYj9si4xULph02bAc2fUXDPMS/g7VfFZcgKuzFvZ/eR3ZHCm";
+    private static final String TRAINED_MODEL = "RelicVuMark";
+    private static final VuforiaLocalizer.CameraDirection CAMERA_DIRECTION = VuforiaLocalizer.CameraDirection.BACK;
+    private static final boolean useExtendedTracking = false;
+    private static final Integer relicID = 0;
+
+    private float robotX = 0;
+    private float robotY = 0;
+    private float robotAngle = 0;
+
+    public void runOpMode() throws InterruptedException {
+        setupVuforia();
+        lastKnownLocation = createMatrix(0, 0, 0, 0, 0, 0);
+        waitForStart();
+
+        // Start tracking the targets
+        visionTargets.activate();
+
+        while(opModeIsActive()) {
+            // Ask the listener for the latest information on where the robot is
+            OpenGLMatrix latestLocation = listener.getUpdatedRobotLocation();
+
+            // The listener will sometimes return null, so we check for that to prevent errors
+            if(latestLocation != null) {
+                lastKnownLocation = latestLocation;
+            }
+
+
+
+
+
+            RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(target);
+            if (vuMark != RelicRecoveryVuMark.UNKNOWN) {
+
+                telemetry.addData("VuMark", "%s visible", vuMark);
+
+                OpenGLMatrix pose = ((VuforiaTrackableDefaultListener)target.getListener()).getPose();
+                telemetry.addData("Pose", formatMatrix(pose));
+
+                if (pose != null) {
+                    VectorF trans = pose.getTranslation();
+                    Orientation rot = Orientation.getOrientation(pose, AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
+
+                    // Extract the X, Y, and Z components of the offset of the target relative to the robot
+                    double tX = trans.get(0);
+                    double tY = trans.get(1);
+                    double tZ = trans.get(2);
+
+                    // Extract the rotational components of the target relative to the robot
+                    double rX = rot.firstAngle;
+                    double rY = rot.secondAngle;
+                    double rZ = rot.thirdAngle;
+
+                }
+            } else {
+                telemetry.addData("VuMark", "not visible");
+            }
+
+            telemetry.update();
+
+
+
+
+
+
+
+
+            float[] coordinates = lastKnownLocation.getTranslation().getData();
+
+            robotX = coordinates[0];
+            robotY = coordinates[1];
+            robotAngle = Orientation.getOrientation(lastKnownLocation, AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).thirdAngle;
+
+            // Send information about whether the target is visible, and where the robot is
+            telemetry.addData("Tracking " + target.getName(), listener.isVisible());
+            telemetry.addData("Last Known Location", formatMatrix(lastKnownLocation));
+
+            // Send telemetry and idle to let hardware catch up
+            telemetry.update();
+            idle();
+        }
+    }
+
+    private void setupVuforia() {
+        parameters = new VuforiaLocalizer.Parameters(R.id.cameraMonitorViewId); // To remove the camera view from the screen, remove the R.id.cameraMonitorViewId
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraDirection = CAMERA_DIRECTION;
+        parameters.useExtendedTracking = useExtendedTracking;
+
+        vuforiaLocalizer = ClassFactory.createVuforiaLocalizer(parameters);
+
+        visionTargets = vuforiaLocalizer.loadTrackablesFromAsset(TRAINED_MODEL);
+        Vuforia.setHint(HINT.HINT_MAX_SIMULTANEOUS_IMAGE_TARGETS, 4);
+
+        // Setup the target to be tracked
+        target = visionTargets.get(relicID); // 0 corresponds to the wheels target
+        target.setName("ID TEST 0");
+        target.setLocation(createMatrix(0, 500, 0, 90, 0, 90));
+
+        // Set phone location on robot
+        phoneLocation = createMatrix(0, 225, 0, 90, 0, 0);
+
+        // Setup listener and inform it of phone information
+        listener = (VuforiaTrackableDefaultListener) target.getListener();
+        listener.setPhoneInformation(phoneLocation, parameters.cameraDirection);
+    }
+
+    // Creates a matrix for determining the locations and orientations of objects
+    // Units are millimeters for x, y, and z, and degrees for u, v, and w
+    private OpenGLMatrix createMatrix(float x, float y, float z, float u, float v, float w) {
+        return OpenGLMatrix.translation(x, y, z).
+                multiplied(Orientation.getRotationMatrix(
+                        AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES, u, v, w));
+    }
+
+    // Formats a matrix into a readable string
+    private String formatMatrix(OpenGLMatrix matrix)
+    {
+        return matrix.formatAsTransform();
+    }
+
+}
